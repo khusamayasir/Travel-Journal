a309165f1723a25feaff434437e7f8f38878fde7
Usama-Yasir <usamayasir32@gmail.com>
2022-04-28 21:05:12 +0500

    updated commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Navbar from "./components/Navbar.js";
+// import Data from "./Data.js"
+
+export default function App() {
+  return (
+    <div>
+      <Navbar />
+    </div>
+  );
+}
